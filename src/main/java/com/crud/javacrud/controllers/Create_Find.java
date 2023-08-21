6a44a6d1e63faba9f882e9fcd5b8b9e20254bf2b
Perera Dev <nashi.codes@gmail.com>
2023-08-21 01:26:09 -0300

    adding controller

---
+++ src/main/java/com/crud/javacrud/controllers/Create_Find.java
@@ -0,0 +1,20 @@
+package com.crud.javacrud.controllers;
+
+import jakarta.servlet.*;
+import jakarta.servlet.annotation.*;
+import jakarta.servlet.http.*;
+
+import java.io.IOException;
+
+@WebServlet(name = "Create_Find", value = "/Create_Find")
+public class Create_Find extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
