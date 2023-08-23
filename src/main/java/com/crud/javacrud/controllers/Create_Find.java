c71487a4777a65c7acd256c80431e2fa916d8f45
Perera Dev <nashi.codes@gmail.com>
2023-08-22 23:08:03 -0300

    updating gitignore, rm and add

--- src/main/java/com/crud/javacrud/controllers/Create_Find.java
+++ src/main/java/com/crud/javacrud/controllers/Create_Find.java
@@ -1,5 +1,7 @@
 package com.crud.javacrud.controllers;
 
+import com.crud.javacrud.dao.ClientDao;
+import com.crud.javacrud.model.Client;
 import jakarta.servlet.*;
 import jakarta.servlet.annotation.*;
 import jakarta.servlet.http.*;
@@ -15,6 +17,13 @@ public class Create_Find extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+            Client client = new Client();
+            client.setName(request.getParameter("nome"));
+            client.setCode(request.getParameter("cpf"));
+            client.setBirthDate(request.getParameter("nascimento"));
+            client.setSituation(request.getParameter("situacao"));
+
+        ClientDao.create(client);
 
     }
 }
--- src/main/java/com/crud/javacrud/dao/ClientDao.java
+++ src/main/java/com/crud/javacrud/dao/ClientDao.java
@@ -1,11 +1,16 @@
 package com.crud.javacrud.dao;
 
+import com.crud.javacrud.db.MySqlConnection;
 import com.crud.javacrud.model.Client;
 
+import java.sql.Connection;
 import java.util.List;
 
 public class ClientDao implements CRUD {
-    static void create(Client client) {
+
+    private static Connection connection = MySqlConnection.getConnection();
+    private static final String SQL_INSERT = "INSERT INTO clients (name, code, birth_date, situation) VALUES (?, ?, ?, ?)";
+    public static void create(Client client) {
 
     }
 
