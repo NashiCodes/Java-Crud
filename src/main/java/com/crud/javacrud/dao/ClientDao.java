d36ca7883b7f742c8314da4246ba2d90dc0ec574
Perera Dev <nashi.codes@gmail.com>
2023-09-20 23:39:28 -0300

    inserting client into database

--- src/main/java/com/crud/javacrud/dao/ClientDao.java
+++ src/main/java/com/crud/javacrud/dao/ClientDao.java
@@ -4,30 +4,46 @@ import com.crud.javacrud.db.MySqlConnection;
 import com.crud.javacrud.model.Client;
 
 import java.sql.Connection;
+import java.sql.SQLException;
 import java.util.List;
 
 public class ClientDao implements CRUD {
 
-    private static Connection connection = MySqlConnection.getConnection();
-    private static final String SQL_INSERT = "INSERT INTO clients (name, code, birth_date, situation) VALUES (?, ?, ?, ?)";
-    public static void create(Client client) {
+	private static  Connection connection;
+	private static String sql;
 
-    }
+	public static void create(Client client) {
+		connection = MySqlConnection.createConnection();
+		sql = "INSERT INTO clients VALUES (null, ?, ?, ?, ?)";
+		try {
+			assert connection != null;
+			var preparedStatement = connection.prepareStatement(sql);
+			preparedStatement.setString(1, client.getName());
+			preparedStatement.setString(2, client.getCode());
+			preparedStatement.setString(3, client.getBirthDate());
+			preparedStatement.setString(4, client.getSituation());
+			preparedStatement.executeUpdate();
 
-    static List<Client> find(String query) {
-        return null;
-    }
+			System.out.println("Client created successfully!");
+		} catch (SQLException e) {
+			System.out.println("Error creating client: " + e.getMessage());
+		}
+	}
 
-    static void delete(int id) {
+	static List<Client> find(String query) {
+		return null;
+	}
 
-    }
+	static void delete(int id) {
 
-    static Client findByPk(int id) {
-        return null;
-    }
+	}
 
-    static void update(Client client) {
+	static Client findByPk(int id) {
+		return null;
+	}
 
-    }
+	static void update(Client client) {
+
+	}
 
 }
